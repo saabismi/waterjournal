818b06e319728037d41bdd9beeeab2c48f881704
saabismi <vilho.syvahuoko@gmail.com>
2021-04-19 10:27:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/waterjournal/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.waterjournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Moi, testaan vaa et näkyyhän tää :D
+}
