b75f75424b810d87ac066cff81f01f4a57c42787
saabismi <vilho.syvahuoko@gmail.com>
2021-04-20 14:37:24 +0300

    created a simple functionality for checking if the user has already registered, and some variables

--- app/src/main/java/com/example/waterjournal/MainActivity.java
+++ app/src/main/java/com/example/waterjournal/MainActivity.java
@@ -2,22 +2,47 @@ package com.example.waterjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences preferences; // create sharedpreferences variable
+    private final String USER_STORE = "UserStore"; // create preferences for storing information about the user, etc.
+    private final String userRegistered = "userRegistered"; // storage for sharing the information about whether the user is registered already or not
+    private final String userWeight  = "userWeight"; // storage for storing the user weight
+    private final String userSex = "userSex"; // storage for storing the user's sex
+    private final String userTarget = "userTarget"; // target water amount per day
+    private boolean getRegistered; // variable for getting the value of registration status key
+    private double getWeight; //variable for getting the value of the weight key
+    private String getSex; // varbiale for getting the value of the sex key
+    private double getTarget; // variable for getting the value of the water target key
+
+    private String TAG = "WaterLog:"; // easy to use tag for logging
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // if(created == true) {
-            // run app
-        // } else {
-            // start registration.java
-        // }
+        preferences = getSharedPreferences(USER_STORE, Context.MODE_PRIVATE);
+        getRegistered = preferences.getBoolean(userRegistered, false);
+        getWeight = preferences.getFloat(userWeight, 0);
+        getSex = preferences.getString(userSex, null);
+        getTarget = preferences.getFloat(userTarget, 0);
+
+        if (getRegistered != true) { // checking if the user has registered already
+            Log.i(TAG, "User hasn't registered yet, let's go to Registration activity");
+            Intent regIntent = new Intent(this, Registration.class); // create intent for going to Registration.java
+            startActivity(regIntent); // start the activity Registration.java
+        } else {
+            Log.i(TAG, "User has already registered, continuing in MainActivity");
+        }
 
     }
 
-    // testi
+
 }
